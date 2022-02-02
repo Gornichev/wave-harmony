f9446c37bd3e428f25fc946882c4674d3cf86b3b
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2022-02-02 15:34:10 +0200

    find btn js

--- int/js/main.js
+++ int/js/main.js
@@ -64,21 +64,14 @@ const intro = new Swiper(".trust-slider", {
     },
 });
 
-
 const searchIcon = document.getElementById('searchIcon')
 const searchBtn = document.getElementById('searchBtn')
-
 
 
 searchIcon.addEventListener('click', function (){
     searchBtn.classList.toggle('hidden-panel')
 
 })
-
-// Certificate-btn
-
-
-// Certificate-btn
 
 // searchBtn
 
