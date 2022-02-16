0dbdd139ba9b95a16de7e07cb9e578f50c911a0f
Gornichev <gornichev1986@gmail.com.noreply.github.com>
2022-02-16 10:31:50 +0200

    tabs catalog

--- int/js/main.js
+++ int/js/main.js
@@ -111,20 +111,16 @@ buttonToggle.forEach(function (item){
 
 const tabHeaders = document.querySelectorAll('.categories-section-item');
 
-
-// Нашли все контент боксы
 const contentBoxes = document.querySelectorAll('[data-tab-content]');
 console.log(contentBoxes)
 
 tabHeaders.forEach(function (item) {
     item.addEventListener('click', function () {
 
-        // 1. Скрыть все content box
         contentBoxes.forEach(function (item) {
             item.classList.add('hidden');
         });
 
-        // 2. Выбрать нужный content box и показать его
         const contentBox = document.querySelector('#' + this.dataset.tab);
         contentBox.classList.remove('hidden');
 
